33cbed6392a688915ff38dd08911e545d181419c
Maksim Makushchenko <maksalmak@gmail.com>
2023-11-23 09:26:59 +0300

    View Mode

--- Editor/Protocol/GroupStateLink.cs
+++ Editor/Protocol/GroupStateLink.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace Packages.tour_creator.Editor.Protocol
 {
+    [Serializable]
     public enum GroupConnectionProtocolViewMode
     {
         ShowByClickOnItem,
